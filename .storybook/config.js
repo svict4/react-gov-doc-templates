616663271178cd688fa611e2c5b82382d23ed728
Simon <email@simonvictory.com>
2018-11-06 00:10:38 +1100

    show addon-info default

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,21 @@
 import { configure } from '@storybook/react';
+import { setDefaults } from "@storybook/addon-info";
+
+setDefaults({
+  header: false,
+  inline: true,
+  maxPropsIntoLine: 1,
+  styles: {
+    infoBody: {
+      border: "none",
+      padding: "0px",
+      borderTop: "1px solid rgb(238, 238, 238)",
+      paddingTop: "20px",
+      marginTop: "25px",
+      boxShadow: "none"
+    }
+  }
+});
 
 function loadStories() {
   require('../src/stories');

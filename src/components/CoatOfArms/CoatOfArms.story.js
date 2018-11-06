1d1dc0f05161f18e094a7cb9ee309a37f008fd61
Simon <email@simonvictory.com>
2018-11-06 19:12:54 +1100

    added stories into storybook for CoA

--- src/components/CoatOfArms/CoatOfArms.story.js
+++ src/components/CoatOfArms/CoatOfArms.story.js
@@ -1,13 +1,84 @@
 import React from "react";
-import { text } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+
 import CoatOfArms from "./";
 
-const CoatOfArmsStory = withInfo({
-    text:
-      "Commonwealth Coat of Arms for the Commonwealth of Australia"
-  })(() =>
-    <CoatOfArms width={text("width", "400px")} />
+storiesOf("Coat of Arms", module)
+  .addDecorator(withKnobs)
+  .addDecorator(withInfo)
+  .add("by itself", () => <CoatOfArms width={text("width", "400px")} />, {
+    info: "Commonwealth Coat of Arms for the Commonwealth of Australia"
+  })
+  .add(
+    "inline",
+    () => (
+      <CoatOfArms
+        width={text("width", "400px")}
+        entityName={text("Entity Name", "Department of Social Services")}
+        stripped={boolean('Stripped', false)}
+        inline
+      />
+    ),
+    {
+      info:
+        "text on the right of the Coat of Arms where 'Australian Government' appears above institution name"
+    }
+  )
+  // TODO
+  .add(
+    "stacked",
+    () => (
+      <CoatOfArms
+        width={text("width", "400px")}
+        entityName={text("Entity Name", "Department of Social Services")}
+        stripped={boolean('Stripped', false)}
+        stacked
+      />
+    ),
+    {
+      info:
+        "text below Coat of Arms where 'Australian Government' appears above institution name"
+    }
+  )
+  // TODO
+  .add(
+    "inverted, on dark",
+    () => (
+      <CoatOfArms
+        width={text("width", "400px")}
+        entityName={text("Entity Name", "Department of Social Services")}
+        stacked
+      />
+    ),
+    {
+      info: "inverted colors for dark backgrounds"
+    }
+  )
+  .add(
+    "co-brand",
+    () => (
+      <CoatOfArms
+        width={text("width", "400px")}
+        entityName={text("Entity Name", "Department of Social Services")}
+        stacked
+      />
+    ),
+    {
+      info: "inverted colors for dark backgrounds"
+    }
+  )
+  .add(
+    "initiative",
+    () => (
+      <CoatOfArms
+        width={text("width", "400px")}
+        entityName={text("Entity Name", "Department of Social Services")}
+        inline
+      />
+    ),
+    {
+      info: "inverted colors for dark backgrounds"
+    }
   );
-  
-  export default CoatOfArmsStory;
